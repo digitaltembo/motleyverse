20582ec58ca2f2231de466d5415474f46bfa5796
Nolan Hawkins <nolanhhawkins@gmail.com>
2023-04-30 10:49:19 -0400

    Allow for loading glsl files in source

--- client/src/world/canvas/Canvas.tsx
+++ client/src/world/canvas/Canvas.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import vertex from "../vertex/base.vs";
+
 const CanvasComponent = styled("canvas")`
   width: 100%;
   height: 100%;
