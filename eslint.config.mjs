548c9ec010eb91498ae0ab20e1fd6092024fa505
Nolan Hawkins <nolanhhawkins@gmail.com>
2025-09-27 12:56:36 -0400

    workspaces?

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["ts/*/dist"]),
+  {
+    files: ["./ts/*/src/**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  tseslint.configs.recommended,
+]);
